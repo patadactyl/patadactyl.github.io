ac89c26db0eed79ed250efb311394fd99129ce61
Patrick Charlton <37526417+patadactyl@users.noreply.github.com>
2021-07-01 16:20:38 -0700

    Contact section hidden until bug addressed

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
